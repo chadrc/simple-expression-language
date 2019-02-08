7bd81e6949b6d2f428bfdb68ebfe17aaf53c0b6f
chadcollins <chadrcollins91@gmail.com>
2019-02-07 19:29:22 -0500

    Implemented tokenizing more than one symbol

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
+pub fn tokenize(input: &String) -> Vec<String> {
+    let mut tokens: Vec<String> = vec![];
 
-pub fn tokenize(input: &String) -> &String {
-    return input;
+    let mut current_token = String::new();
+    for c in input.chars() {
+        if c.is_whitespace() {
+            continue;
+        }
+
+        current_token.push(c);
+
+        if current_token.len() > 0 {
+            tokens.push(current_token);
+            current_token = String::new();
+        }
+    }
+
+    return tokens;
 }
 
 #[cfg(test)]
@@ -9,8 +24,19 @@ mod tests {
 
     #[test]
     fn tokenize_integer_expression() {
-        let input = &String::from("4");
-        let result = tokenize(input);
-        assert_eq!(result, "4");
+        let input = String::from("4");
+        let result = tokenize(&input);
+        assert_eq!(*result.get(0).unwrap(), "4");
+    }
+
+    #[test]
+    fn tokenize_addition_expression() {
+        let input = String::from("4 + 5");
+        let result = tokenize(&input);
+
+        assert_eq!(result.len(), 3);
+        assert_eq!(*result.get(0).unwrap(), "4");
+        assert_eq!(*result.get(1).unwrap(), "+");
+        assert_eq!(*result.get(2).unwrap(), "5");
     }
 }
