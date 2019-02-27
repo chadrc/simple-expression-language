c3fa38c5dd3435014dac48b829d634fbc7181999
chadcollins <chadrcollins91@gmail.com>
2019-02-27 05:46:29 -0500

    Compile addition operation

--- sel_compiler/src/lib.rs
+++ sel_compiler/src/lib.rs
@@ -1,13 +1,15 @@
-use sel_tokenizer::{TokenType, Tokenizer};
+use sel_tokenizer::{Token, TokenType, Tokenizer};
 
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum Operation {
     Touch,
+    Addition,
     None,
 }
 
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum DataType {
+    Unknown,
     Unit,
     Integer,
     Decimal,
@@ -26,19 +28,54 @@ impl Value {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct SELTreeNode {
     operation: Operation,
     value: Value,
+    left: Box<Option<SELTreeNode>>,
+    right: Box<Option<SELTreeNode>>,
 }
 
+const LEFT_OP_INDEX: usize = 0;
+const RIGHT_OP_INDEX: usize = 1;
+
 impl SELTreeNode {
+    fn new(op: Operation, data_type: DataType) -> Self {
+        return SELTreeNode {
+            operation: op,
+            value: Value {
+                data_type: data_type,
+            },
+            // largest operation has two operands
+            left: Box::new(None),
+            right: Box::new(None),
+        };
+    }
+
     pub fn get_operation(&self) -> Operation {
         return self.operation;
     }
 
     pub fn get_value(&self) -> Value {
         return self.value;
+    }
+
+    pub fn get_left(&self) -> Option<&SELTreeNode> {
+        return match &*self.left {
+            None => None,
+            Some(n) => Some(&n),
+        };
+    }
+
+    // fn set_left(&mut self, node: SELTreeNode) {
+    //     self.
+    // }
+
+    pub fn get_right(&self) -> Option<&SELTreeNode> {
+        return match &*self.right {
+            None => None,
+            Some(n) => Some(&n),
+        };
     }
 }
 
@@ -48,8 +85,8 @@ pub struct SELTree {
 }
 
 impl SELTree {
-    pub fn get_root(&self) -> SELTreeNode {
-        return self.root;
+    pub fn get_root(&self) -> &SELTreeNode {
+        return &self.root;
     }
 }
 
@@ -61,41 +98,95 @@ impl Compiler {
     }
 
     pub fn compile(&self, s: &String) -> SELTree {
-        let tokenizer = Tokenizer::new(s);
+        let mut tokenizer = Tokenizer::new(s);
 
-        // assume some defaults
-        let mut op = Operation::None;
-        let mut data_type = DataType::Unit;
+        let mut last_node: Option<&SELTreeNode> = None;
+        let mut last_op_node: Option<SELTreeNode> = None;
 
-        for token in tokenizer {
-            let token_type = token.get_token_type();
-            if token_type == TokenType::Integer {
-                op = Operation::Touch;
-                data_type = DataType::Integer;
-            } else if token_type == TokenType::Decimal {
-                op = Operation::Touch;
-                data_type = DataType::Decimal;
-            } else if token_type == TokenType::SingleQuotedString
-                || token_type == TokenType::DoubleQuotedString
-                || token_type == TokenType::FormattedString
-            {
-                op = Operation::Touch;
-                data_type = DataType::String;
-            } else if token_type == TokenType::Boolean {
-                op = Operation::Touch;
-                data_type = DataType::Boolean;
+        // process first token
+        let token = match tokenizer.next() {
+            None => {
+                return SELTree {
+                    root: SELTreeNode::new(Operation::None, DataType::Unit),
+                };
             }
-        }
-
-        return SELTree {
-            root: SELTreeNode {
-                operation: op,
-                value: Value {
-                    data_type: data_type,
-                },
-            },
+            Some(t) => t,
         };
+
+        let token_type = token.get_token_type();
+
+        // first token should be a value token
+        let op = Operation::Touch;
+        let data_type = get_value_type_for_token(token);
+
+        let first_node = SELTreeNode::new(op, data_type);
+
+        // second token should be a operation token
+        let token = match tokenizer.next() {
+            None => {
+                // no other operations
+                // return the touch operation
+                return SELTree { root: first_node };
+            }
+            Some(t) => t,
+        };
+
+        let op = get_operation_type_for_token(token);
+        let data_type = DataType::Unknown;
+
+        let mut op_node = SELTreeNode::new(op, data_type);
+
+        // place first token as left operand
+
+        op_node.left = Box::new(Some(first_node));
+
+        // get next token and place it as right operand of op_node
+        let token = match tokenizer.next() {
+            None => {
+                // no second operand
+                // return incomplete operation
+                return SELTree { root: op_node };
+            }
+            Some(t) => t,
+        };
+
+        // first token should be a value token
+        let op = Operation::Touch;
+        let data_type = get_value_type_for_token(token);
+
+        let second_node = SELTreeNode::new(op, data_type);
+
+        op_node.right = Box::new(Some(second_node));
+
+        return SELTree { root: op_node };
     }
+}
+
+fn get_value_type_for_token(token: Token) -> DataType {
+    let token_type = token.get_token_type();
+
+    return if token_type == TokenType::Integer {
+        DataType::Integer
+    } else if token_type == TokenType::Decimal {
+        DataType::Decimal
+    } else if token_type == TokenType::SingleQuotedString
+        || token_type == TokenType::DoubleQuotedString
+        || token_type == TokenType::FormattedString
+    {
+        DataType::String
+    } else if token_type == TokenType::Boolean {
+        DataType::Boolean
+    } else {
+        DataType::Unknown
+    };
+}
+
+fn get_operation_type_for_token(token: Token) -> Operation {
+    return if token.get_token_type() == TokenType::PlusSign {
+        Operation::Addition
+    } else {
+        Operation::None
+    };
 }
 
 #[cfg(test)]
@@ -109,7 +200,12 @@ mod tests {
 
     #[test]
     fn compiles_empty() {
-        let root = get_compiled_root("");
+        let input = String::from("");
+        let compiler = Compiler::new();
+
+        let tree = compiler.compile(&input);
+
+        let root = tree.get_root();
 
         assert_eq!(root.get_operation(), Operation::None);
         assert_eq!(root.get_value().get_type(), DataType::Unit);
@@ -117,7 +213,12 @@ mod tests {
 
     #[test]
     fn compiles_touch_integer() {
-        let root = get_compiled_root("9");
+        let input = String::from("9");
+        let compiler = Compiler::new();
+
+        let tree = compiler.compile(&input);
+
+        let root = tree.get_root();
 
         assert_eq!(root.get_operation(), Operation::Touch);
         assert_eq!(root.get_value().get_type(), DataType::Integer);
@@ -125,7 +226,12 @@ mod tests {
 
     #[test]
     fn compiles_touch_decimal() {
-        let root = get_compiled_root("3.14");
+        let input = String::from("3.14");
+        let compiler = Compiler::new();
+
+        let tree = compiler.compile(&input);
+
+        let root = tree.get_root();
 
         assert_eq!(root.get_operation(), Operation::Touch);
         assert_eq!(root.get_value().get_type(), DataType::Decimal);
@@ -133,7 +239,12 @@ mod tests {
 
     #[test]
     fn compiles_touch_single_quote_string() {
-        let root = get_compiled_root("'hello world'");
+        let input = String::from("'hello world'");
+        let compiler = Compiler::new();
+
+        let tree = compiler.compile(&input);
+
+        let root = tree.get_root();
 
         assert_eq!(root.get_operation(), Operation::Touch);
         assert_eq!(root.get_value().get_type(), DataType::String);
@@ -141,7 +252,12 @@ mod tests {
 
     #[test]
     fn compiles_touch_double_quote_string() {
-        let root = get_compiled_root("\"hello world\"");
+        let input = String::from("\"hello world\"");
+        let compiler = Compiler::new();
+
+        let tree = compiler.compile(&input);
+
+        let root = tree.get_root();
 
         assert_eq!(root.get_operation(), Operation::Touch);
         assert_eq!(root.get_value().get_type(), DataType::String);
@@ -149,7 +265,12 @@ mod tests {
 
     #[test]
     fn compiles_touch_formatted_string() {
-        let root = get_compiled_root("`hello world`");
+        let input = String::from("`hello world`");
+        let compiler = Compiler::new();
+
+        let tree = compiler.compile(&input);
+
+        let root = tree.get_root();
 
         assert_eq!(root.get_operation(), Operation::Touch);
         assert_eq!(root.get_value().get_type(), DataType::String);
@@ -157,20 +278,36 @@ mod tests {
 
     #[test]
     fn compiles_touch_boolean() {
-        let root = get_compiled_root("true");
+        let input = String::from("true");
+        let compiler = Compiler::new();
+
+        let tree = compiler.compile(&input);
+
+        let root = tree.get_root();
 
         assert_eq!(root.get_operation(), Operation::Touch);
         assert_eq!(root.get_value().get_type(), DataType::Boolean);
     }
 
-    // Helpers
-
-    fn get_compiled_root(s: &str) -> SELTreeNode {
-        let input = String::from(s);
+    #[test]
+    fn compiles_addition_operation() {
+        let input = String::from("5 + 10");
         let compiler = Compiler::new();
 
         let tree = compiler.compile(&input);
 
-        return tree.get_root();
+        let root = tree.get_root();
+
+        let left = root.get_left();
+        let right = root.get_right();
+
+        assert_eq!(root.get_operation(), Operation::Addition);
+        assert_eq!(root.get_value().get_type(), DataType::Unknown);
+
+        assert_eq!(left.unwrap().get_operation(), Operation::Touch);
+        assert_eq!(left.unwrap().get_value().get_type(), DataType::Integer);
+
+        assert_eq!(right.unwrap().get_operation(), Operation::Touch);
+        assert_eq!(right.unwrap().get_value().get_type(), DataType::Integer);
     }
 }
