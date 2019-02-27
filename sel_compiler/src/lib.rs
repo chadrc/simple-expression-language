33e412e8baa4072bb009c0ee3f6d38a6f415838f
chadcollins <chadrcollins91@gmail.com>
2019-02-27 06:36:27 -0500

    Compiling multiplication sign

--- sel_compiler/src/lib.rs
+++ sel_compiler/src/lib.rs
@@ -4,6 +4,7 @@ use sel_tokenizer::{Token, TokenType, Tokenizer};
 pub enum Operation {
     Touch,
     Addition,
+    Multiplication,
     None,
 }
 
@@ -218,6 +219,8 @@ fn get_value_type_for_token(token: Token) -> DataType {
 fn get_operation_type_for_token(token: Token) -> Operation {
     return if token.get_token_type() == TokenType::PlusSign {
         Operation::Addition
+    } else if token.get_token_type() == TokenType::MultiplicationSign {
+        Operation::Multiplication
     } else {
         Operation::None
     };
@@ -346,6 +349,28 @@ mod tests {
     }
 
     #[test]
+    fn compiles_multiplication_operation() {
+        let input = String::from("5 * 10");
+        let compiler = Compiler::new();
+
+        let tree = compiler.compile(&input);
+
+        let root = tree.get_root();
+
+        let left = root.get_left();
+        let right = root.get_right();
+
+        assert_eq!(root.get_operation(), Operation::Multiplication);
+        assert_eq!(root.get_value().get_type(), DataType::Unknown);
+
+        assert_eq!(left.unwrap().get_operation(), Operation::Touch);
+        assert_eq!(left.unwrap().get_value().get_type(), DataType::Integer);
+
+        assert_eq!(right.unwrap().get_operation(), Operation::Touch);
+        assert_eq!(right.unwrap().get_value().get_type(), DataType::Integer);
+    }
+
+    #[test]
     fn compiles_two_addition_operations() {
         let input = String::from("5 + 10 + 15");
         let compiler = Compiler::new();
@@ -382,4 +407,42 @@ mod tests {
         assert_eq!(l2_right.get_operation(), Operation::Touch);
         assert_eq!(l2_right.get_value().get_type(), DataType::Integer);
     }
+
+    // #[test]
+    // fn compiles_addition_multiplication_operations() {
+    //     let input = String::from("5 + 10 * 15");
+    //     let compiler = Compiler::new();
+
+    //     let tree = compiler.compile(&input);
+
+    //     // tree should look like
+    //     //          +
+    //     //         / \
+    //     //        +   15
+    //     //       / \
+    //     //      5   10
+
+    //     let root = tree.get_root();
+
+    //     let left = root.get_left().unwrap();
+    //     let right = root.get_right().unwrap();
+
+    //     let l2_left = left.get_left().unwrap();
+    //     let l2_right = left.get_right().unwrap();
+
+    //     assert_eq!(root.get_operation(), Operation::Addition);
+    //     assert_eq!(root.get_value().get_type(), DataType::Unknown);
+
+    //     assert_eq!(left.get_operation(), Operation::Addition);
+    //     assert_eq!(left.get_value().get_type(), DataType::Unknown);
+
+    //     assert_eq!(right.get_operation(), Operation::Touch);
+    //     assert_eq!(right.get_value().get_type(), DataType::Integer);
+
+    //     assert_eq!(l2_left.get_operation(), Operation::Touch);
+    //     assert_eq!(l2_left.get_value().get_type(), DataType::Integer);
+
+    //     assert_eq!(l2_right.get_operation(), Operation::Touch);
+    //     assert_eq!(l2_right.get_value().get_type(), DataType::Integer);
+    // }
 }
