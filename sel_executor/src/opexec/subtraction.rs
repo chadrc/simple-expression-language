d43831c6edf9f6517bdb7a3e6401587b9a34a884
chadcollins <chadrcollins91@gmail.com>
2019-03-09 16:30:59 -0500

    Executing decimal integer subtractions

--- sel_executor/src/opexec/subtraction.rs
+++ sel_executor/src/opexec/subtraction.rs
@@ -19,6 +19,30 @@ pub fn subtraction_operation(tree: &SELTree, node: &SELTreeNode) -> SELExecution
 
             SELExecutionResult::new(DataType::Integer, Some(to_byte_vec(result)))
         }
+        (DataType::Integer, DataType::Decimal) => {
+            let (left_val, right_val) =
+                get_values_from_results::<i32, f64>(&left_result, &right_result);
+
+            let result = f64::from(left_val) - right_val;
+
+            SELExecutionResult::new(DataType::Decimal, Some(to_byte_vec(result)))
+        }
+        (DataType::Decimal, DataType::Integer) => {
+            let (left_val, right_val) =
+                get_values_from_results::<f64, i32>(&left_result, &right_result);
+
+            let result = left_val - f64::from(right_val);
+
+            SELExecutionResult::new(DataType::Decimal, Some(to_byte_vec(result)))
+        }
+        (DataType::Decimal, DataType::Decimal) => {
+            let (left_val, right_val) =
+                get_values_from_results::<f64, f64>(&left_result, &right_result);
+
+            let result = left_val - right_val;
+
+            SELExecutionResult::new(DataType::Decimal, Some(to_byte_vec(result)))
+        }
         _ => SELExecutionResult::new(DataType::Unknown, Some(vec![])),
     };
 }
@@ -45,5 +69,62 @@ mod tests {
 
         assert_eq!(result.get_type(), DataType::Integer);
         assert_eq!(result_value, Some(4));
+    }
+
+    #[test]
+    fn executes_integer_decimal_subtraction() {
+        let result = result_of_binary_op(
+            Operation::Subtraction,
+            DataType::Integer,
+            "9",
+            DataType::Decimal,
+            "3.14",
+        );
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Decimal);
+        assert_eq!(result_value, Some(9.0 - 3.14));
+    }
+
+    #[test]
+    fn executes_decimal_integer_addition() {
+        let result = result_of_binary_op(
+            Operation::Subtraction,
+            DataType::Decimal,
+            "3.14",
+            DataType::Integer,
+            "9",
+        );
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Decimal);
+        assert_eq!(result_value, Some(3.14 - 9.0));
+    }
+
+    #[test]
+    fn executes_decimal_addition() {
+        let result = result_of_binary_op(
+            Operation::Subtraction,
+            DataType::Decimal,
+            "3.14",
+            DataType::Decimal,
+            "6.45",
+        );
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Decimal);
+        assert_eq!(result_value, Some(3.14 - 6.45));
     }
 }
