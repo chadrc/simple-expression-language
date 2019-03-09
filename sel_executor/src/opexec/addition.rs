1916715617c13f3691289cf362cdcdef367894f5
chadcollins <chadrcollins91@gmail.com>
2019-03-09 13:36:21 -0500

    Moved operation tests to be with the function

--- sel_executor/src/opexec/addition.rs
+++ sel_executor/src/opexec/addition.rs
@@ -62,3 +62,141 @@ pub fn addition_operation(tree: &SELTree, node: &SELTreeNode) -> SELExecutionRes
 
     return SELExecutionResult::new(result_type, result);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::get_node_result;
+    use sel_common::{from_byte_vec, DataHeap, DataType, Operation, SELTree, SELTreeNode};
+
+    #[test]
+    fn executes_integer_addition() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        let mut heap = DataHeap::new();
+
+        let mut left = SELTreeNode::new(
+            Operation::Touch,
+            DataType::Integer,
+            0,
+            heap.insert_from_string(DataType::Integer, &String::from("9")),
+        );
+
+        let mut right = SELTreeNode::new(
+            Operation::Touch,
+            DataType::Integer,
+            1,
+            heap.insert_from_string(DataType::Integer, &String::from("5")),
+        );
+
+        let mut root = SELTreeNode::new(Operation::Addition, DataType::Unknown, 2, None);
+
+        left.set_parent(Some(2));
+        right.set_parent(Some(2));
+
+        root.set_left(Some(0));
+        root.set_right(Some(1));
+
+        nodes.push(left);
+        nodes.push(right);
+        nodes.push(root);
+
+        let tree = SELTree::new(2, nodes, heap);
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Integer);
+        assert_eq!(result_value, Some(14));
+    }
+
+    #[test]
+    fn executes_integer_decimal_addition() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        let mut heap = DataHeap::new();
+
+        let mut left = SELTreeNode::new(
+            Operation::Touch,
+            DataType::Integer,
+            0,
+            heap.insert_from_string(DataType::Integer, &String::from("9")),
+        );
+
+        let mut right = SELTreeNode::new(
+            Operation::Touch,
+            DataType::Decimal,
+            1,
+            heap.insert_from_string(DataType::Decimal, &String::from("3.14")),
+        );
+
+        let mut root = SELTreeNode::new(Operation::Addition, DataType::Unknown, 2, None);
+
+        left.set_parent(Some(2));
+        right.set_parent(Some(2));
+
+        root.set_left(Some(0));
+        root.set_right(Some(1));
+
+        nodes.push(left);
+        nodes.push(right);
+        nodes.push(root);
+
+        let tree = SELTree::new(2, nodes, heap);
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Decimal);
+        assert_eq!(result_value, Some(12.14));
+    }
+
+    #[test]
+    fn executes_decimal_integer_addition() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        let mut heap = DataHeap::new();
+
+        let mut left = SELTreeNode::new(
+            Operation::Touch,
+            DataType::Decimal,
+            0,
+            heap.insert_from_string(DataType::Decimal, &String::from("3.14")),
+        );
+
+        let mut right = SELTreeNode::new(
+            Operation::Touch,
+            DataType::Integer,
+            1,
+            heap.insert_from_string(DataType::Integer, &String::from("9")),
+        );
+
+        let mut root = SELTreeNode::new(Operation::Addition, DataType::Unknown, 2, None);
+
+        left.set_parent(Some(2));
+        right.set_parent(Some(2));
+
+        root.set_left(Some(0));
+        root.set_right(Some(1));
+
+        nodes.push(left);
+        nodes.push(right);
+        nodes.push(root);
+
+        let tree = SELTree::new(2, nodes, heap);
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Decimal);
+        assert_eq!(result_value, Some(12.14));
+    }
+}
--- sel_executor/src/opexec/touch.rs
+++ sel_executor/src/opexec/touch.rs
@@ -10,3 +10,126 @@ pub fn touch_operation(tree: &SELTree, node: &SELTreeNode) -> SELExecutionResult
         _ => SELExecutionResult::new(DataType::Unknown, None),
     };
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::get_node_result;
+    use sel_common::{from_byte_vec, DataHeap, DataType, Operation, SELTree, SELTreeNode};
+
+    #[test]
+    fn executes_unit_touch() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        nodes.push(SELTreeNode::new(Operation::Touch, DataType::Unit, 0, None));
+
+        let tree = SELTree::new(0, nodes, DataHeap::new());
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        assert_eq!(result.get_type(), DataType::Unit);
+        assert_eq!(result.get_value(), None);
+    }
+
+    #[test]
+    fn executes_integer_touch() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        let mut heap = DataHeap::new();
+
+        let value = heap.insert_from_string(DataType::Integer, &String::from("9"));
+        nodes.push(SELTreeNode::new(
+            Operation::Touch,
+            DataType::Integer,
+            0,
+            value,
+        ));
+
+        let tree = SELTree::new(0, nodes, heap);
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Integer);
+        assert_eq!(result_value, Some(9));
+    }
+
+    #[test]
+    fn executes_decimal_touch() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        let mut heap = DataHeap::new();
+
+        let value = heap.insert_from_string(DataType::Decimal, &String::from("3.14"));
+        nodes.push(SELTreeNode::new(
+            Operation::Touch,
+            DataType::Decimal,
+            0,
+            value,
+        ));
+
+        let tree = SELTree::new(0, nodes, heap);
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Decimal);
+        assert_eq!(result_value, Some(3.14));
+    }
+
+    #[test]
+    fn executes_string_touch() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        let mut heap = DataHeap::new();
+
+        let value = heap.insert_from_string(DataType::String, &String::from("Hello World"));
+        nodes.push(SELTreeNode::new(
+            Operation::Touch,
+            DataType::String,
+            0,
+            value,
+        ));
+
+        let tree = SELTree::new(0, nodes, heap);
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::String);
+        assert_eq!(result_value, Some(String::from("Hello World")));
+    }
+
+    #[test]
+    fn executes_boolean_touch() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        let mut heap = DataHeap::new();
+
+        let value = heap.insert_from_string(DataType::Boolean, &String::from("true"));
+        nodes.push(SELTreeNode::new(
+            Operation::Touch,
+            DataType::Boolean,
+            0,
+            value,
+        ));
+
+        let tree = SELTree::new(0, nodes, heap);
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Boolean);
+        assert_eq!(result_value, Some(true));
+    }
+}
