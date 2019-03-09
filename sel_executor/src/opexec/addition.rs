b82e8095ec98b252f4f8607fcc8406751e113c0c
chadcollins <chadrcollins91@gmail.com>
2019-03-09 13:40:50 -0500

    Executes decimal addition

--- sel_executor/src/opexec/addition.rs
+++ sel_executor/src/opexec/addition.rs
@@ -57,6 +57,21 @@ pub fn addition_operation(tree: &SELTree, node: &SELTreeNode) -> SELExecutionRes
 
             (Some(to_byte_vec(result)), DataType::Decimal)
         }
+        (DataType::Decimal, DataType::Decimal) => {
+            let left_int: Option<f64> = match left_result.get_value() {
+                Some(value) => Some(from_byte_vec(value)),
+                None => None,
+            };
+
+            let right_int: Option<f64> = match right_result.get_value() {
+                Some(value) => Some(from_byte_vec(value)),
+                None => None,
+            };
+
+            let result = left_int.unwrap() + right_int.unwrap();
+
+            (Some(to_byte_vec(result)), DataType::Decimal)
+        }
         _ => (Some(vec![]), DataType::Unknown),
     };
 
@@ -198,5 +213,49 @@ mod tests {
 
         assert_eq!(result.get_type(), DataType::Decimal);
         assert_eq!(result_value, Some(12.14));
+    }
+
+    #[test]
+    fn executes_decimal_addition() {
+        let mut nodes: Vec<SELTreeNode> = vec![];
+        let mut heap = DataHeap::new();
+
+        let mut left = SELTreeNode::new(
+            Operation::Touch,
+            DataType::Decimal,
+            0,
+            heap.insert_from_string(DataType::Decimal, &String::from("3.14")),
+        );
+
+        let mut right = SELTreeNode::new(
+            Operation::Touch,
+            DataType::Decimal,
+            1,
+            heap.insert_from_string(DataType::Decimal, &String::from("6.45")),
+        );
+
+        let mut root = SELTreeNode::new(Operation::Addition, DataType::Unknown, 2, None);
+
+        left.set_parent(Some(2));
+        right.set_parent(Some(2));
+
+        root.set_left(Some(0));
+        root.set_right(Some(1));
+
+        nodes.push(left);
+        nodes.push(right);
+        nodes.push(root);
+
+        let tree = SELTree::new(2, nodes, heap);
+
+        let result = get_node_result(&tree, tree.get_root());
+
+        let result_value = match result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(result.get_type(), DataType::Decimal);
+        assert_eq!(result_value, Some(9.59));
     }
 }
