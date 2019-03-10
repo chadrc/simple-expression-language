af03b60826b82c0d9c2ecdbd88c00af5d5795abe
chadcollins <chadrcollins91@gmail.com>
2019-03-10 13:22:29 -0400

    Implemented debug for result

--- sel_executor/src/opexec/execution_result.rs
+++ sel_executor/src/opexec/execution_result.rs
@@ -25,27 +25,28 @@ impl SELExecutionResult {
     }
 }
 
+impl std::fmt::Debug for SELExecutionResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?} - {}", self.data_type, self)
+    }
+}
+
 impl std::fmt::Display for SELExecutionResult {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let none_str = String::from("None");
 
-        let mut val_str = match self.get_value() {
-            Some(val) => match self.data_type {
-                DataType::String => format!("\"{}\"", from_byte_vec::<String>(val)),
-                DataType::Integer => format!("{}", from_byte_vec::<i32>(val)),
-                DataType::Decimal => format!("{}", from_byte_vec::<f64>(val)),
-                DataType::Boolean => format!("{}", from_byte_vec::<bool>(val)),
-                DataType::Unit => String::from("()"),
-                _ => none_str,
-            },
+        let val = self.get_value();
+
+        let val_str = match self.data_type {
+            DataType::String => format!("\"{}\"", from_byte_vec::<String>(val.unwrap())),
+            DataType::Integer => format!("{}", from_byte_vec::<i32>(val.unwrap())),
+            DataType::Decimal => format!("{}", from_byte_vec::<f64>(val.unwrap())),
+            DataType::Boolean => format!("{}", from_byte_vec::<bool>(val.unwrap())),
+            DataType::Unit => String::from("()"),
             _ => none_str,
         };
 
-        if self.data_type == DataType::Unit {
-            val_str = String::from("()");
-        }
-
-        write!(f, "{} - {}", self.data_type, val_str)
+        write!(f, "{}", val_str)
     }
 }
 
@@ -63,7 +64,7 @@ mod tests {
 
         let formatted = format!("{}", result);
 
-        assert_eq!(formatted, "String - \"Hello World\"");
+        assert_eq!(formatted, "\"Hello World\"");
     }
 
     #[test]
@@ -72,7 +73,7 @@ mod tests {
 
         let formatted = format!("{}", result);
 
-        assert_eq!(formatted, "Integer - 10");
+        assert_eq!(formatted, "10");
     }
 
     #[test]
@@ -81,7 +82,7 @@ mod tests {
 
         let formatted = format!("{}", result);
 
-        assert_eq!(formatted, "Decimal - 3.14");
+        assert_eq!(formatted, "3.14");
     }
 
     #[test]
@@ -90,7 +91,7 @@ mod tests {
 
         let formatted = format!("{}", result);
 
-        assert_eq!(formatted, "Boolean - false");
+        assert_eq!(formatted, "false");
     }
 
     #[test]
@@ -98,6 +99,54 @@ mod tests {
         let result = SELExecutionResult::new(DataType::Unit, None);
 
         let formatted = format!("{}", result);
+
+        assert_eq!(formatted, "()");
+    }
+
+    #[test]
+    fn debug_str() {
+        let result = SELExecutionResult::new(
+            DataType::String,
+            Some(to_byte_vec(&String::from("Hello World"))),
+        );
+
+        let formatted = format!("{:?}", result);
+
+        assert_eq!(formatted, "String - \"Hello World\"");
+    }
+
+    #[test]
+    fn debug_int() {
+        let result = SELExecutionResult::new(DataType::Integer, Some(to_byte_vec(10)));
+
+        let formatted = format!("{:?}", result);
+
+        assert_eq!(formatted, "Integer - 10");
+    }
+
+    #[test]
+    fn debug_decimal() {
+        let result = SELExecutionResult::new(DataType::Decimal, Some(to_byte_vec(3.14)));
+
+        let formatted = format!("{:?}", result);
+
+        assert_eq!(formatted, "Decimal - 3.14");
+    }
+
+    #[test]
+    fn debug_bool() {
+        let result = SELExecutionResult::new(DataType::Boolean, Some(to_byte_vec(false)));
+
+        let formatted = format!("{:?}", result);
+
+        assert_eq!(formatted, "Boolean - false");
+    }
+
+    #[test]
+    fn debug_unit() {
+        let result = SELExecutionResult::new(DataType::Unit, None);
+
+        let formatted = format!("{:?}", result);
 
         assert_eq!(formatted, "Unit - ()");
     }
