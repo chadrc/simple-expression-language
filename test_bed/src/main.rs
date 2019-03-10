c9a1ba4cd92309a325bc7c5caf00ac51d6de4c4b
chadcollins <chadrcollins91@gmail.com>
2019-03-10 13:28:45 -0400

    Trying stuff in test_bed

--- test_bed/src/main.rs
+++ test_bed/src/main.rs
@@ -1,25 +1,25 @@
-use sel_common;
 use sel_compiler;
 use sel_executor;
 // use sel_tokenizer;
 
 fn main() {
-    let input = String::from("5 + 400 * 8 - 3");
+    let inputs = [
+        "5 + 400 * 8 - 3",
+        "3 > 5 || 89 != 43",
+        "2 * 3.14",
+        "'Hello ' + 'World'",
+    ];
     // let tokenizer = sel_tokenizer::Tokenizer::new(&input);
     // for token in tokenizer {
     //     println!("{:?}", token);
     // }
 
     let compiler = sel_compiler::Compiler::new();
-    let tree = compiler.compile(&input);
 
-    let result = sel_executor::execute_sel_tree(tree);
+    for input in inputs.iter() {
+        let tree = compiler.compile(&String::from(*input));
 
-    let result_val: Option<i32> = match result.get_value() {
-        Some(val) => Some(sel_common::from_byte_vec(val)),
-        None => None,
-    };
-
-    println!("result: {:?}", result.get_type());
-    println!("result: {:?}", result_val);
+        let result = sel_executor::execute_sel_tree(tree);
+        println!("{}", result);
+    }
 }
