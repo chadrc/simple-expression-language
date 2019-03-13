0f0d31de12281160cc0560aebcacb157609ea0a4
chadcollins <chadrcollins91@gmail.com>
2019-03-13 14:44:33 -0400

    Added test for differing priorities on either side of a group

--- sel_compiler/src/tests/groups.rs
+++ sel_compiler/src/tests/groups.rs
@@ -149,3 +149,49 @@ fn single_group_begin_end() {
     assert_eq!(lr_right.get_operation(), Operation::Touch);
     assert_eq!(lr_right.get_data_type(), DataType::Integer);
 }
+
+#[test]
+fn single_group_begin_end_lower_begin() {
+    let input = String::from("5 + (5 + 10) * 15");
+    let compiler = Compiler::new();
+
+    let tree = compiler.compile(&input);
+
+    // tree should look like
+    //            +
+    //           / \
+    //          5   *
+    //             / \
+    //            +  15
+    //           / \
+    //          5  10
+
+    let root = tree.get_root();
+
+    let left = tree.get_nodes().get(root.get_left().unwrap()).unwrap();
+    let right = tree.get_nodes().get(root.get_right().unwrap()).unwrap();
+
+    let r_left = tree.get_nodes().get(right.get_left().unwrap()).unwrap();
+    let r_right = tree.get_nodes().get(right.get_right().unwrap()).unwrap();
+
+    let rl_left = tree.get_nodes().get(r_left.get_left().unwrap()).unwrap();
+    let rl_right = tree.get_nodes().get(r_left.get_right().unwrap()).unwrap();
+
+    assert_eq!(root.get_operation(), Operation::Addition);
+
+    assert_eq!(left.get_operation(), Operation::Touch);
+    assert_eq!(left.get_data_type(), DataType::Integer);
+
+    assert_eq!(right.get_operation(), Operation::Multiplication);
+
+    assert_eq!(r_left.get_operation(), Operation::Addition);
+
+    assert_eq!(r_right.get_operation(), Operation::Touch);
+    assert_eq!(r_right.get_data_type(), DataType::Integer);
+
+    assert_eq!(rl_left.get_operation(), Operation::Touch);
+    assert_eq!(rl_left.get_data_type(), DataType::Integer);
+
+    assert_eq!(rl_right.get_operation(), Operation::Touch);
+    assert_eq!(rl_right.get_data_type(), DataType::Integer);
+}
