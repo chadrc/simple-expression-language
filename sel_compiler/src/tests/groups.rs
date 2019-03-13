75147bfcbd837b9ba49497c33ef9772f2ae31729
chadcollins <chadrcollins91@gmail.com>
2019-03-13 13:21:00 -0400

    Added test for group with no surrounding ops

--- sel_compiler/src/tests/groups.rs
+++ sel_compiler/src/tests/groups.rs
@@ -2,6 +2,33 @@ use super::super::Compiler;
 use sel_common::{DataType, Operation};
 
 #[test]
+fn single_group() {
+    let input = String::from("(5 + 10)");
+    let compiler = Compiler::new();
+
+    let tree = compiler.compile(&input);
+
+    // tree should look like
+    //          +
+    //         / \
+    //        5  10
+
+    let root = tree.get_root();
+
+    let left = tree.get_nodes().get(root.get_left().unwrap()).unwrap();
+    let right = tree.get_nodes().get(root.get_right().unwrap()).unwrap();
+
+    assert_eq!(root.get_operation(), Operation::Addition);
+    assert_eq!(root.get_data_type(), DataType::Unknown);
+
+    assert_eq!(left.get_operation(), Operation::Touch);
+    assert_eq!(left.get_data_type(), DataType::Integer);
+
+    assert_eq!(right.get_operation(), Operation::Touch);
+    assert_eq!(right.get_data_type(), DataType::Integer);
+}
+
+#[test]
 fn single_group_end() {
     let input = String::from("5 * (10 + 15)");
     let compiler = Compiler::new();
