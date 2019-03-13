793eb1c5042ade081e95b497fe4d221fc163e089
chadcollins <chadrcollins91@gmail.com>
2019-03-13 08:24:36 -0400

    Implemented multiple results from tree execution

--- sel_executor/src/lib.rs
+++ sel_executor/src/lib.rs
@@ -7,10 +7,21 @@ pub use context::SELValue;
 pub use opexec::SELExecutionResult;
 use sel_common::{DataType, SELTree};
 
-pub fn execute_sel_tree(tree: &SELTree, context: &SELContext) -> SELExecutionResult {
+pub fn execute_sel_tree(tree: &SELTree, context: &SELContext) -> Vec<SELExecutionResult> {
+    let mut results: Vec<SELExecutionResult> = vec![];
+
     if tree.get_nodes().len() > 0 {
-        return opexec::get_node_result(tree, tree.get_root(), context);
+        results.push(opexec::get_node_result(tree, tree.get_root(), context));
+
+        for sub_root_index in 0..tree.get_sub_roots().len() {
+            match tree.get_sub_root(sub_root_index) {
+                Some(sub_root) => results.push(opexec::get_node_result(tree, sub_root, context)),
+                None => (),
+            }
+        }
+    } else {
+        results.push(SELExecutionResult::new(DataType::Unknown, None));
     }
 
-    return SELExecutionResult::new(DataType::Unknown, None);
+    return results;
 }
--- sel_executor/src/tests/mod.rs
+++ sel_executor/src/tests/mod.rs
@@ -2,7 +2,7 @@
 mod tree_execution {
     use super::super::execute_sel_tree;
     use super::super::SELContext;
-    use sel_common::{DataHeap, DataType, SELTree};
+    use sel_common::{from_byte_vec, DataHeap, DataType, SELTree};
     use sel_compiler::Compiler;
 
     #[test]
@@ -11,12 +11,39 @@ mod tree_execution {
 
         let context = SELContext::new();
 
-        let result = execute_sel_tree(&tree, &context);
+        let results = execute_sel_tree(&tree, &context);
+
+        let result = results.get(0).unwrap();
 
         assert_eq!(result.get_type(), DataType::Unknown);
         assert_eq!(result.get_value(), None);
     }
 
     #[test]
-    fn multiple_results() {}
+    fn multiple_results() {
+        let compiler = Compiler::new();
+        let tree = compiler.compile(&String::from("5 + 10\n15 + 20"));
+
+        let context = SELContext::new();
+
+        let results = execute_sel_tree(&tree, &context);
+
+        let first_result = results.get(0).unwrap();
+        let first_result_value = match first_result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(first_result.get_type(), DataType::Integer);
+        assert_eq!(first_result_value, Some(15));
+
+        let second_result = results.get(1).unwrap();
+        let second_result_value = match second_result.get_value() {
+            Some(value) => Some(from_byte_vec(value)),
+            None => None,
+        };
+
+        assert_eq!(second_result.get_type(), DataType::Integer);
+        assert_eq!(second_result_value, Some(35));
+    }
 }
