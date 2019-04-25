bfb1e27070846def49d766cb2b418194abb771b6
chadcollins <chadrcollins91@gmail.com>
2019-04-25 12:01:19 -0400

    Implemented start/end group for precedence manager

--- sel_compiler/src/precedence_manager.rs
+++ sel_compiler/src/precedence_manager.rs
@@ -15,9 +15,38 @@ const EQUALITY_PRECEDENCE: usize = RELATIONAL_PRECEDENCE + 1;
 const AND_PRECEDENCE: usize = EQUALITY_PRECEDENCE + 1;
 const OR_PRECEDENCE: usize = AND_PRECEDENCE + 1;
 
+pub struct PrecedenceGroup {
+    parent: usize,
+    members: Vec<Vec<usize>>,
+}
+
+impl PrecedenceGroup {
+    pub fn new(parent: usize) -> Self {
+        let mut members: Vec<Vec<usize>> = vec![];
+
+        members.push(vec![]); // VALUE_PRECEDENCE
+        members.push(vec![]); // START_GROUP_PRECEDENCE
+        members.push(vec![]); // END_GROUP_PRECEDENCE
+        members.push(vec![]); // UNARY_PRECEDENCE
+        members.push(vec![]); // RANGE_PRECEDENCE
+        members.push(vec![]); // EXPONENTIAL_PRECEDENCE
+        members.push(vec![]); // MULTIPLICATION_PRECEDENCE
+        members.push(vec![]); // ADDITION_PRECEDENCE
+        members.push(vec![]); // RELATIONAL_PRECEDENCE
+        members.push(vec![]); // EQUALITY_PRECEDENCE
+        members.push(vec![]); // AND_PRECEDENCE
+        members.push(vec![]); // OR_PRECEDENCE
+
+        return PrecedenceGroup { members, parent };
+    }
+}
+
 pub struct PrecedenceManager {
     operation_priorities: HashMap<Operation, usize>,
-    precedence_buckets: Vec<Vec<usize>>,
+    //    precedence_buckets: Vec<Vec<usize>>,
+    precedence_groups: Vec<Vec<PrecedenceGroup>>,
+    current_tier: usize,
+    current_group: usize,
 }
 
 impl PrecedenceManager {
@@ -58,39 +87,69 @@ impl PrecedenceManager {
 
         operation_priorities.insert(Operation::LogicalOr, OR_PRECEDENCE);
 
-        let mut precedence_buckets: Vec<Vec<usize>> = vec![];
+        let mut root_group_tier: Vec<PrecedenceGroup> = vec![];
+        root_group_tier.push(PrecedenceGroup::new(0));
 
-        precedence_buckets.push(vec![]); // VALUE_PRECEDENCE
-        precedence_buckets.push(vec![]); // START_GROUP_PRECEDENCE
-        precedence_buckets.push(vec![]); // END_GROUP_PRECEDENCE
-        precedence_buckets.push(vec![]); // UNARY_PRECEDENCE
-        precedence_buckets.push(vec![]); // RANGE_PRECEDENCE
-        precedence_buckets.push(vec![]); // EXPONENTIAL_PRECEDENCE
-        precedence_buckets.push(vec![]); // MULTIPLICATION_PRECEDENCE
-        precedence_buckets.push(vec![]); // ADDITION_PRECEDENCE
-        precedence_buckets.push(vec![]); // RELATIONAL_PRECEDENCE
-        precedence_buckets.push(vec![]); // EQUALITY_PRECEDENCE
-        precedence_buckets.push(vec![]); // AND_PRECEDENCE
-        precedence_buckets.push(vec![]); // OR_PRECEDENCE
+        let mut precedence_groups: Vec<Vec<PrecedenceGroup>> = vec![];
+        precedence_groups.push(root_group_tier);
 
         return PrecedenceManager {
             operation_priorities,
-            precedence_buckets,
+            //            precedence_buckets,
+            precedence_groups,
+            current_tier: 0,
+            current_group: 0,
         };
     }
 
+    fn last_group_in_current_tier(&self) -> usize {
+        return self.precedence_groups.get(self.current_tier).unwrap().len() - 1;
+    }
+
+    fn current_group(&self) -> &PrecedenceGroup {
+        return self
+            .precedence_groups
+            .get(self.current_tier)
+            .unwrap()
+            .get(self.last_group_in_current_tier())
+            .unwrap();
+    }
+
+    fn current_group_mut(&mut self) -> &mut PrecedenceGroup {
+        let last_group = { self.last_group_in_current_tier() };
+        return self
+            .precedence_groups
+            .get_mut(self.current_tier)
+            .unwrap()
+            .get_mut(last_group)
+            .unwrap();
+    }
+
     pub fn get_buckets(&self) -> &Vec<Vec<usize>> {
-        return &self.precedence_buckets;
+        return &self.current_group().members;
+    }
+
+    pub fn get_group_tiers(&self) -> &Vec<Vec<PrecedenceGroup>> {
+        return &self.precedence_groups;
     }
 
     pub fn get_start_group_bucket(&self) -> &Vec<usize> {
-        return self.precedence_buckets.get(START_GROUP_PRECEDENCE).unwrap();
+        return self
+            .current_group()
+            .members
+            .get(START_GROUP_PRECEDENCE)
+            .unwrap();
     }
 
     pub fn get_end_group_bucket(&self) -> &Vec<usize> {
-        return self.precedence_buckets.get(END_GROUP_PRECEDENCE).unwrap();
+        return self
+            .current_group()
+            .members
+            .get(END_GROUP_PRECEDENCE)
+            .unwrap();
     }
 
+    // TODO: make static along with op priorities struct
     pub fn is_op_value_precedence(&self, op: Operation) -> bool {
         return match self.operation_priorities.get(&op) {
             None => false,
@@ -102,17 +161,43 @@ impl PrecedenceManager {
     }
 
     pub fn add_index_with_operation(&mut self, op: Operation, index: usize) {
-        match self.operation_priorities.get(&op) {
+        let precedence = *{ self.operation_priorities.get(&op).unwrap() };
+
+        match self.current_group_mut().members.get_mut(precedence) {
             None => (),
-            Some(precedence) => match self.precedence_buckets.get_mut(*precedence) {
-                None => (),
-                Some(bucket) => {
-                    bucket.push(index);
-                }
-            },
+            Some(bucket) => {
+                bucket.push(index);
+            }
         }
     }
 
+    pub fn start_group(&mut self) {
+        // parent of new group is always the last group of the current tier
+        let parent = self.last_group_in_current_tier();
+
+        // update and fetch new current tier
+        self.current_tier += 1;
+        let tier = match self.precedence_groups.get_mut(self.current_tier) {
+            Some(t) => t,
+            None => {
+                // need to create tier
+                self.precedence_groups.push(vec![]);
+
+                // now exists, safe to unwrap
+                self.precedence_groups.get_mut(self.current_tier).unwrap()
+            }
+        };
+
+        // add new group
+        tier.push(PrecedenceGroup::new(parent));
+    }
+
+    pub fn end_group(&mut self) {
+        // ending a group always means returning to previous tier
+        self.current_tier -= 1;
+    }
+
+    // TODO: make static along with op priorities struct
     pub fn is_lower(&self, op: Operation, relative_to: Operation) -> bool {
         let op_precedence = match self.operation_priorities.get(&op) {
             None => return false,
@@ -149,5 +234,35 @@ mod tests {
 
         assert_eq!(value_bucket.len(), 1);
         assert_eq!(*value_bucket.get(0).unwrap(), 0);
+    }
+
+    #[test]
+    fn start_group() {
+        let mut manager = PrecedenceManager::new();
+        manager.add_index_with_operation(Operation::Touch, 0);
+
+        manager.start_group();
+
+        manager.add_index_with_operation(Operation::Touch, 1);
+        manager.add_index_with_operation(Operation::Touch, 2);
+
+        manager.end_group();
+
+        let group_tiers = manager.get_group_tiers();
+
+        assert_eq!(group_tiers.len(), 2);
+        assert_eq!(group_tiers.get(1).unwrap().get(0).unwrap().parent, 0);
+        assert_eq!(
+            group_tiers
+                .get(1)
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .members
+                .get(VALUE_PRECEDENCE)
+                .unwrap()
+                .len(),
+            2
+        );
     }
 }
