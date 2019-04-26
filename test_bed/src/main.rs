daa3496c049329ddbd577317b18cfc03b0e10739
chadcollins <chadrcollins91@gmail.com>
2019-04-26 18:45:40 -0400

    Added inclusive range to test bed

--- test_bed/src/main.rs
+++ test_bed/src/main.rs
@@ -16,7 +16,10 @@ fn main() {
         'Input is: ' + $
         ",
         "5.0 * (8 + 2) / (3 + (9 - 4)) * ((4 + 7) * 3)",
-        "5..100",
+        "
+        0..100
+        0...100
+        ",
     ];
     // let tokenizer = sel_tokenizer::Tokenizer::new(&input);
     // for token in tokenizer {
