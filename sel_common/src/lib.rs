8860eb2ae5860fd9a8e350bd5c91769ed4c9e2a8
chadcollins <chadrcollins91@gmail.com>
2019-04-28 17:41:17 -0400

    Moved Pair to submodule

--- sel_common/src/lib.rs
+++ sel_common/src/lib.rs
@@ -5,20 +5,20 @@ mod context;
 mod data_heap;
 mod data_type;
 mod operation;
-mod pair;
 mod sel_tree;
 mod sel_value;
 mod symbol_table;
+mod types;
 mod utils;
 
 pub use context::SELContext;
 pub use data_heap::DataHeap;
 pub use data_type::DataType;
 pub use operation::Operation;
-pub use pair::Pair;
 pub use sel_tree::{NodeSide, SELTree, SELTreeNode};
 pub use sel_value::SELValue;
 pub use symbol_table::SymbolTable;
+pub use types::Pair;
 pub use utils::{from_byte_vec, to_byte_vec, FromByteVec, ToByteVec};
 
 #[cfg(test)]
