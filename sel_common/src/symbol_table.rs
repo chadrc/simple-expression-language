a7c0036ba0b1a94295c74a31ed4aa10d5f497788
chadcollins <chadrcollins91@gmail.com>
2019-04-28 07:59:05 -0400

    Tests for symbol table Made sure adding same symbol multiple times yields the same value

--- sel_common/src/symbol_table.rs
+++ sel_common/src/symbol_table.rs
@@ -15,11 +15,16 @@ impl SymbolTable {
     }
 
     pub fn add(&mut self, symbol: &String) -> usize {
-        let index = self.symbols.len();
-        self.symbols.push(symbol.clone());
-        self.key_to_index.insert(symbol.clone(), index);
+        return match self.key_to_index.get(symbol) {
+            Some(value) => *value,
+            None => {
+                let index = self.symbols.len();
+                self.symbols.push(symbol.clone());
+                self.key_to_index.insert(symbol.clone(), index);
 
-        index
+                index
+            }
+        };
     }
 
     pub fn get_symbol(&self, value: usize) -> Option<&String> {
@@ -28,5 +33,32 @@ impl SymbolTable {
 
     pub fn get_value(&self, symbol: &String) -> Option<&usize> {
         return self.key_to_index.get(symbol);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SymbolTable;
+
+    #[test]
+    fn create() {
+        SymbolTable::new();
+    }
+
+    #[test]
+    fn add() {
+        let mut table = SymbolTable::new();
+        table.add(&String::from("symbol1"));
+
+        assert_eq!(table.symbols.len(), 1);
+    }
+
+    #[test]
+    fn add_same() {
+        let mut table = SymbolTable::new();
+        table.add(&String::from("symbol1"));
+        table.add(&String::from("symbol1"));
+
+        assert_eq!(table.symbols.len(), 1);
     }
 }
