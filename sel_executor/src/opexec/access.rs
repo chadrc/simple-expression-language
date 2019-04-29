cda68cdd845bf4db02bd7fff169f47aef774f149
chadcollins <chadrcollins91@gmail.com>
2019-04-29 12:32:42 -0400

    Accessing list by index returns value

--- sel_executor/src/opexec/access.rs
+++ sel_executor/src/opexec/access.rs
@@ -3,8 +3,34 @@ use crate::opexec::execution_result::SELExecutionResult;
 use crate::opexec::get_node_result;
 use crate::opexec::utils::get_left_right_results;
 use sel_common::{
-    from_byte_vec, to_byte_vec, DataType, Operation, Pair, SELTree, SELTreeNode, SELValue,
+    from_byte_vec, to_byte_vec, DataType, List, Operation, Pair, SELTree, SELTreeNode, SELValue,
 };
+
+fn get_identifier(node: &SELTreeNode, tree: &SELTree) -> String {
+    node.get_right()
+        .and_then(|index| tree.get_nodes().get(index))
+        .and_then(|node| {
+            if node.get_data_type() == DataType::Identifier {
+                tree.get_usize_value_of(node)
+                    .and_then(|symbol_index| tree.get_symbol_table().get_symbol(symbol_index))
+            } else {
+                None
+            }
+        })
+        .map_or(String::from(""), |s| s.clone())
+}
+
+fn get_index(node: &SELTreeNode, tree: &SELTree) -> Option<usize> {
+    node.get_right()
+        .and_then(|index| tree.get_nodes().get(index))
+        .and_then(|node| {
+            if node.get_data_type() == DataType::Integer {
+                tree.get_usize_value_of(node)
+            } else {
+                None
+            }
+        })
+}
 
 pub fn dot_access_operation(
     tree: &SELTree,
@@ -19,23 +45,11 @@ pub fn dot_access_operation(
         .map(|node| get_node_result(tree, node, context))
         .unwrap_or(SELExecutionResult::new(DataType::Unknown, None));
 
-    // get type and raw value of right
-    // should be an identifier
-    let identifier: String = node
-        .get_right()
-        .and_then(|index| tree.get_nodes().get(index))
-        .and_then(|node| {
-            if node.get_data_type() == DataType::Identifier {
-                tree.get_usize_value_of(node)
-                    .and_then(|symbol_index| tree.get_symbol_table().get_symbol(symbol_index))
-            } else {
-                None
-            }
-        })
-        .map_or(String::from(""), |s| s.clone());
-
     return match left_result.get_type() {
         DataType::Pair => {
+            // get type and raw value of right
+            // should be an identifier
+            let identifier: String = get_identifier(node, tree);
             let pair: Pair = from_byte_vec(left_result.get_value().unwrap());
             match identifier.as_ref() {
                 "left" => SELExecutionResult::from(pair.get_left()),
@@ -43,6 +57,13 @@ pub fn dot_access_operation(
                 _ => SELExecutionResult::new(DataType::Unit, None),
             }
         }
+        DataType::List => match get_index(node, tree) {
+            Some(index) => {
+                let list: List = from_byte_vec(left_result.get_value().unwrap());
+                SELExecutionResult::from(list.get_values().get(index).unwrap_or(&SELValue::new()))
+            }
+            None => SELExecutionResult::new(DataType::Unit, None),
+        },
         _ => SELExecutionResult::new(DataType::Unit, None),
     };
 }
@@ -126,5 +147,18 @@ mod tests {
 
         assert_eq!(result.get_type(), DataType::Integer);
         assert_eq!(value, 100);
+    }
+
+    #[test]
+    fn executes_list_index_access() {
+        let compiler = Compiler::new();
+        let tree = compiler.compile(&String::from("(100, 200, 300).1"));
+        let execution_context = SELExecutionContext::new();
+
+        let result = get_node_result(&tree, tree.get_root(), &execution_context);
+        let value: i64 = from_byte_vec(result.get_value().unwrap());
+
+        assert_eq!(result.get_type(), DataType::Integer);
+        assert_eq!(value, 200);
     }
 }
