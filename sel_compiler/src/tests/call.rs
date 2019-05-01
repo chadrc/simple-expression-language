42bb4d865347b1dda4856e2f05ab3fa8ba87e274
chadcollins <chadrcollins91@gmail.com>
2019-05-01 05:03:57 -0400

    Test for call op with succeeding op

--- sel_compiler/src/tests/call.rs
+++ sel_compiler/src/tests/call.rs
@@ -84,3 +84,39 @@ fn single_argument_call_preceding_op() {
     assert_eq!(r_right.get_operation(), Operation::Touch);
     assert_eq!(r_right.get_data_type(), DataType::Integer);
 }
+
+#[test]
+fn single_argument_call_succeeding_op() {
+    let input = String::from("get_vars(5) + 5");
+    let compiler = Compiler::new();
+
+    let tree = compiler.compile(&input);
+
+    // tree should look like
+    //             __ + __
+    //            /       \
+    //         _ G _      5
+    //        /     \
+    //   get_vars   5
+
+    let root = tree.get_root();
+
+    let left = tree.get_nodes().get(root.get_left().unwrap()).unwrap();
+    let right = tree.get_nodes().get(root.get_right().unwrap()).unwrap();
+
+    let l_left = tree.get_nodes().get(left.get_left().unwrap()).unwrap();
+    let l_right = tree.get_nodes().get(left.get_right().unwrap()).unwrap();
+
+    assert_eq!(root.get_operation(), Operation::Addition);
+
+    assert_eq!(right.get_operation(), Operation::Touch);
+    assert_eq!(right.get_data_type(), DataType::Integer);
+
+    assert_eq!(left.get_operation(), Operation::Group);
+
+    assert_eq!(l_left.get_operation(), Operation::Touch);
+    assert_eq!(l_left.get_data_type(), DataType::Identifier);
+
+    assert_eq!(l_right.get_operation(), Operation::Touch);
+    assert_eq!(l_right.get_data_type(), DataType::Integer);
+}
